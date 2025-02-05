18b661ff054c8668c7cf1e4663ec3afe372c130e
DatLaid <81822483+DatNguyen2711@users.noreply.github.com>
2025-02-05 07:31:12 +0000

    add IMeterFactory

---
+++ Medicine-Web/Project231/Models/ContosoMetrics.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.Metrics;
+
+public class ContosoMetrics
+{
+    private readonly Counter<int> _productSoldCounter;
+
+    public ContosoMetrics(IMeterFactory meterFactory)
+    {
+        var meter = meterFactory.Create("Contoso.Web");
+        _productSoldCounter = meter.CreateCounter<int>("contoso.product.sold");
+    }
+
+    public void UserLogin(string email, int quantity)
+    {
+        _productSoldCounter.Add(quantity,
+            new KeyValuePair<string, object?>("contoso.product.name", email));
+    }
+}
--- Medicine-Web/Project231/Program.cs
+++ Medicine-Web/Project231/Program.cs
@@ -2,9 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Project231.Models;
 using Project231.Services;
+using OpenTelemetry.Metrics;
+using Microsoft.AspNetCore.Http.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<ContosoMetrics>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -42,7 +45,20 @@ builder.Services.AddCors(opts =>
     builder.AllowAnyHeader().AllowAnyMethod().
     AllowCredentials().SetIsOriginAllowed((host) => true));
 });
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
 
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -51,7 +67,34 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapPrometheusScrapingEndpoint();
 
+
+                     
+app.Use(async (context, next) =>
+{
+    var tagsFeature = context.Features.Get<IHttpMetricsTagsFeature>();
+    if (tagsFeature != null)
+    {
+        var source = context.Request.Query["utm_medium"].ToString() switch
+        {
+            "" => "none",
+            "social" => "social",
+            "email" => "email",
+            "organic" => "organic",
+            _ => "other"
+        };
+        tagsFeature.Tags.Add(new KeyValuePair<string, object?>("mkt_medium", source));
+    }
+
+    await next.Invoke();
+});
+
+
+
+
+app.MapGet("/", () => "Hello OpenTelemetry! ticks:"
+                     + DateTime.Now.Ticks.ToString()[^3..]);
 app.UseHttpsRedirection();
 app.UseCors("CORSPolicy");
 
